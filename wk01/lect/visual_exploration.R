1491cbba45bcad603445787c5df14f511f010493
Papp Zoltan <pzoltan@gmail.com>
2018-01-08 19:06:01 +0100

    Update plots

--- wk01/lect/visual_exploration.R
+++ wk01/lect/visual_exploration.R
@@ -35,11 +35,8 @@ ggplot(train, aes(x=temp)) + geom_density() + facet_grid(. ~ season)
 
 traindt = data.table(train)
 season_summary <- traindt[, .(count = mean(count)), by=.(season, hour)]
+weather_summary <- traindt[, .(count = mean(count)), by=.(weather, hour)]
 
-##################################
-
-season_summary <- ddply(train,.(season,hour),
-                        summarise, count = mean(count))
 ggplot(train, aes(x = hour, y = count, colour = season)) +
   geom_point(data = season_summary, aes(group = season)) +
   geom_line(data = season_summary, aes(group = season)) +
@@ -49,8 +46,6 @@ ggplot(train, aes(x = hour, y = count, colour = season)) +
   ggtitle("People rent bikes more in Fall, and much less in Spring.\n") + 
   theme(plot.title=element_text(size=18))
 
-weather_summary <- ddply(train,.(weather,hour),
-                         summarise, count = mean(count))
 ggplot(train, aes(x = hour, y = count, colour = weather)) +
   geom_point(data = weather_summary, aes(group = weather)) +
   geom_line(data = weather_summary, aes(group = weather)) +
@@ -58,54 +53,6 @@ ggplot(train, aes(x = hour, y = count, colour = weather)) +
   scale_y_continuous("Count") +
   theme_minimal() +
   ggtitle("People rent bikes more when the weather is Good.\n") + 
-  theme(plot.title=element_text(size=18))
-
-
-day_summary <- ddply(train,.(Weekday,hour),
-                     summarise, count = mean(count))
-ggplot(train, aes(x = hour, y = count, colour = Weekday)) +
-  geom_point(data = day_summary, aes(group=Weekday)) +
-  geom_line(data = day_summary, aes(group=Weekday)) +
-  scale_x_discrete("Hour") +
-  scale_y_continuous("Count") +
-  theme_minimal() +
-  ggtitle("People rent bikes for morning/evening commutes on weekdays,
-          and daytime rides on weekends\n")
-
-
-weather_prob <- ddply(train,.(season, hour),
-                      summarise, Good = mean(weather == "Good"),
-                      Normal = mean(weather == "Normal"),
-                      Bad = mean(weather == "Bad"),
-                      Very_bad = mean(weather == "Very Bad"))
-
-weather_prob
-
-ggplot(train, aes(x = hour, y = Good, colour = season)) +
-  geom_point(data = weather_prob, aes(group = season)) +
-  geom_line(data = weather_prob, aes(group = season)) +
-  scale_x_discrete("Hour") +
-  scale_y_continuous("Prob of Good") +
-  theme_minimal() +
-  ggtitle("The probability of Good weather is higher in Fall. \n") + 
-  theme(plot.title=element_text(size=18))
-
-ggplot(train, aes(x = hour, y = Normal, colour = season)) +
-  geom_point(data = weather_prob, aes(group = season)) +
-  geom_line(data = weather_prob, aes(group = season)) +
-  scale_x_discrete("Hour") +
-  scale_y_continuous("Prob of Normal") +
-  theme_minimal() +
-  ggtitle("The probability of Normal weather is higher in Spring. \n") + 
-  theme(plot.title=element_text(size=18))
-
-ggplot(train, aes(x = hour, y = Bad, colour = season)) +
-  geom_point(data = weather_prob, aes(group = season)) +
-  geom_line(data = weather_prob, aes(group = season)) +
-  scale_x_discrete("Hour") +
-  scale_y_continuous("Prob of Bad") +
-  theme_minimal() +
-  ggtitle("The probability of Bad weather is higher in Summer and Winter. \n") + 
   theme(plot.title=element_text(size=18))
 
 
